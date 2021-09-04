fb917105bc7958bc1d38abcfe67a453e8c4c79ed
Spenser Austin <adactuslatem@gmail.com>
2021-09-04 23:02:15 +0100

    update upload script

--- upload.sh
+++ upload.sh
@@ -2,8 +2,11 @@
 
 echo "Uploading from pi to github..."
 
+echo "Moving files"
+cp -v  ~/printer_config/printer.cfg ./cr10s/printer.cfg
+
 echo "Adding modifications to printer.cfg to git"
-git add /home/pi/printer_config/printer.cfg
+git add ./cr10s/printer.cfg
 git commit -m "Uploaded calibration settings"
 
 echo "Pushing..."
